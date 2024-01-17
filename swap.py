ff306937410a924c6e664f4e73d93614b98c5112
Sahana B S <20ise0078@vvce.ac.in>
2024-01-17 14:06:16 +0530

    project

---
+++ swap.py
@@ -0,0 +1,11 @@
+P = 5 
+Q = 10
+print("value of P=5")
+print("value of Q=10")
+# To Swap the values of two variables using Addition and subtraction operator  
+P = P + Q    
+Q = P - Q   
+P = P - Q  
+   
+print ("The Value of P after swapping: ", P)  
+print ("The Value of Q after swapping: ", Q) 
